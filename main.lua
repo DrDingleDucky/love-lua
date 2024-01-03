8d8b428b8900e18f48fc52652a28869dac5d9b26
George Davidson <georgedavidson392@gmail.com>
2024-01-03 12:01:08 -0500

    moving shape :)

--- main.lua
+++ main.lua
@@ -1,3 +1,24 @@
+function love.load()
+    w = 0
+    h = 0
+    x = 0
+    y = 0
+    t = 0
+end
+
+function love.update(dt)
+    if (t <= 0) then
+        w = math.random(50, 200)
+        h = math.random(50, 200)
+        x = love.graphics.getWidth() / 2 - w / 2
+        y = love.graphics.getHeight() / 2 - h / 2
+        t = 1
+    else
+        t = t - dt
+    end
+end
+
 function love.draw()
-    love.graphics.print("u mun gay lol", 400, 300)
+    love.graphics.setColor(0, 0.4, 0.4)
+    love.graphics.rectangle("fill", x, y, w, h)
 end
