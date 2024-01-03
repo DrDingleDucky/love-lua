3dc4233ee4d0f49c36af3d89d0d609f33d6fb42e
George Davidson <georgedavidson392@gmail.com>
2024-01-03 12:41:37 -0500

    removed global vars

--- main.lua
+++ main.lua
@@ -1,34 +1,31 @@
--- this function is called exactly once at the beginning of the game
-function love.load()
-    w = 0
-    h = 0
-    x = 0
-    y = 0
-    t = 0
-end
+local window_x = 0
+local window_y = 0
+local rect_width = 20
+local rect_height = 20
+local timer = 0
 
 -- callback function triggered when a key is pressed
-function love.keypressed( k )
-    if k == "escape" then
-        love.event.push("quit")
+function love.keypressed(key)
+    if key == "escape" then
+        love.event.push("quit")        
     end
 end
 
 -- callback function used to update the state of the game every frame
 function love.update(dt)
-    if (t <= 0) then
-        w = math.random(50, 200)
-        h = math.random(50, 200)
-        x = love.graphics.getWidth() / 2 - w / 2
-        y = love.graphics.getHeight() / 2 - h / 2
-        t = 1
+    if timer <= 0 then
+        rect_width = math.random(50, 200)
+        rect_height = math.random(50, 200)
+        window_x = love.graphics.getWidth() / 2 - rect_width / 2
+        window_y = love.graphics.getHeight() / 2 - rect_height / 2
+        timer = 1
     else
-        t = t - dt
+        timer = timer - dt
     end
 end
-
+    
 -- callback function used to draw on the screen every frame
 function love.draw()
-    love.graphics.setColor(0, 0.4, 0.4)
-    love.graphics.rectangle("fill", x, y, w, h)
+    love.graphics.setColor(1, 0.4, 0.4)
+    love.graphics.rectangle("fill", window_x, window_y, rect_width, rect_height)
 end
