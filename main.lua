facadcaa4f5efaa46e628443564eab2a493750c4
George Davidson <georgedavidson392@gmail.com>
2024-01-03 12:12:16 -0500

    added comments

--- main.lua
+++ main.lua
@@ -1,3 +1,4 @@
+-- this function is called exactly once at the beginning of the game
 function love.load()
     w = 0
     h = 0
@@ -6,12 +7,14 @@ function love.load()
     t = 0
 end
 
+-- callback function triggered when a key is pressed
 function love.keypressed( k )
     if k == "escape" then
         love.event.push("quit")
     end
 end
 
+-- callback function used to update the state of the game every frame
 function love.update(dt)
     if (t <= 0) then
         w = math.random(50, 200)
@@ -24,6 +27,7 @@ function love.update(dt)
     end
 end
 
+-- callback function used to draw on the screen every frame
 function love.draw()
     love.graphics.setColor(0, 0.4, 0.4)
     love.graphics.rectangle("fill", x, y, w, h)
