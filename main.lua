c2db3026e6a24708280c7e09311308e962ab6265
George Davidson <georgedavidson392@gmail.com>
2024-01-04 11:46:20 -0500

    stash

--- main.lua
+++ main.lua
@@ -1,54 +1,35 @@
-local window_x = 0
-local window_y = 0
-local rect_width = 0
-local rect_height = 0
-local timer = 0
+local rect_x = love.graphics.getWidth() / 2
+local rect_y = love.graphics.getHeight() / 2
+local rect_width = 48
+local rect_height = 96
 local back_color = {1, 0.5, 0.5}
 local rect_color = {0.5, 0.5, 1}
-local back_switch = true
-local rect_switch = true
+local dx = 0
+
+function love.load()
+    love.window.setTitle("Switch Runner")
+    love.window.setMode(1200, 860)
+end
 
 -- callback function triggered when a key is pressed
-function love.keypressed(key)
+function love.keypressed(key, scancode, isrepeat)
     if key == "escape" then
-        love.event.push("quit")
+        love.event.quit()
     end
 end
 
 -- callback function triggered when a mouse button is pressed
 function love.mousepressed(x, y, button, istouch)
-    if button == 1 then
-        if rect_switch then
-            rect_color = {0.5, 0.5, 1}
-        else
-            rect_color = {1, 0.5, 0.5}
-        end
-        rect_switch = not rect_switch
-    end
- end
+end
 
 -- callback function used to update the state of the game every frame
 function love.update(dt)
-    if timer <= 0 then
-        rect_width = math.random(50, 200)
-        rect_height = math.random(50, 200)
-        window_x = love.graphics.getWidth() / 2 - rect_width / 2
-        window_y = love.graphics.getHeight() / 2 - rect_height / 2
-        if back_switch then
-            back_color = {1, 0.5, 0.5}
-        else
-            back_color = {0.5, 0.5, 1}
-        end
-        back_switch = not back_switch
-        timer = 1
-    else
-        timer = timer - dt
-    end
+    rect_x = rect_x + dx * 100 * dt
 end
 
 -- callback function used to draw on the screen every frame
 function love.draw()
     love.graphics.setBackgroundColor(back_color)
     love.graphics.setColor(rect_color)
-    love.graphics.rectangle("fill", window_x, window_y, rect_width, rect_height)
+    love.graphics.rectangle("fill", rect_x, rect_y, rect_width, rect_height)
 end
